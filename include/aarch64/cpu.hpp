66871f1436e1cf21b222a74e2d885637a6d40341
Zone.N <zone.niuzh@hotmail.com>
2025-04-07 16:24:22 +0800

    feat: aarch64 smc psci support
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- include/aarch64/cpu.hpp
+++ include/aarch64/cpu.hpp
@@ -74,11 +74,34 @@ static __always_inline auto GetCurrentCoreId() -> size_t {
  */
 static __always_inline void SetupFpu() { CPACR_EL1::Fpen::Set(); }
 
-static __always_inline void SecureMonitorCall(uint64_t a0, uint64_t a1,
+/**
+ * @brief SMC 调用返回值
+ */
+struct SMCReturnValue {
+  uint64_t a0;
+  uint64_t a1;
+  uint64_t a2;
+  uint64_t a3;
+};
+
+/**
+ * @brief 调用安全监控
+ * @param a0 参数0
+ * @param a1 参数1
+ * @param a2 参数2
+ * @param a3 参数3
+ * @param a4 参数4
+ * @param a5 参数5
+ * @param a6 参数6
+ * @param a7 参数7
+ * @param res 返回值
+ */
+static __always_inline auto SecureMonitorCall(uint64_t a0, uint64_t a1,
                                               uint64_t a2, uint64_t a3,
                                               uint64_t a4, uint64_t a5,
-                                              uint64_t a6, uint64_t a7,
-                                              struct Arm_smccc_res *res) {
+                                              uint64_t a6, uint64_t a7)
+    -> const SMCReturnValue {
+  SMCReturnValue ret;
   register uint64_t x0 __asm__("x0") = a0;
   register uint64_t x1 __asm__("x1") = a1;
   register uint64_t x2 __asm__("x2") = a2;
@@ -87,7 +110,6 @@ static __always_inline void SecureMonitorCall(uint64_t a0, uint64_t a1,
   register uint64_t x5 __asm__("x5") = a5;
   register uint64_t x6 __asm__("x6") = a6;
   register uint64_t x7 __asm__("x7") = a7;
-
   __asm__ volatile(
       "smc #0\n"
       "stp x0, x1, [%8]\n"
@@ -95,8 +117,9 @@ static __always_inline void SecureMonitorCall(uint64_t a0, uint64_t a1,
       : "=r"(x0), "=r"(x1), "=r"(x2), "=r"(x3), "=r"(x4), "=r"(x5), "=r"(x6),
         "=r"(x7)
       : "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5), "r"(x6), "r"(x7),
-        "r"(res)
+        "r"(&ret)
       : "memory");
+  return ret;
 }
 
 /**
@@ -138,8 +161,8 @@ static constexpr const uint64_t kSTAT_RESIDENCY_64 = 0xC4000010;
 static constexpr const uint64_t kSTAT_COUNT_32 = 0x84000011;
 static constexpr const uint64_t kSTAT_COUNT_64 = 0xC4000011;
 
-/// 返回值
-enum ReturnValue {
+/// 错误码
+enum ErrorCode {
   SUCCESS = 0,
   NOT_SUPPORTED = -1,
   INVALID_PARAMETERS = -2,
@@ -150,13 +173,6 @@ enum ReturnValue {
   NOT_PRESENT = -7,
   DISABLED = -8,
   INVALID_ADDRESS = -9,
-};
-
-struct Arm_smccc_res {
-  uint64_t a0;  // 返回值 0
-  uint64_t a1;  // 返回值 1
-  uint64_t a2;  // 返回值 2
-  uint64_t a3;  // 返回值 3
 };
 
 /// 电源状态编码
@@ -211,13 +227,12 @@ struct PowerState {
  * @param power_state 电源状态
  * @param entry_point_address 用于恢复的地址
  * @param context_id 上下文标识
- * @return ReturnValue 返回值
+ * @return ErrorCode 错误码
  * @see DEN0022F.b_Power_State_Coordination_Interface.pdf#5.1.2
  */
 static __always_inline auto CpuSuspend(PowerState power_state,
                                        uint64_t entry_point_address,
-                                       uint64_t context_id)
-    -> enum ReturnValue {
+                                       uint64_t context_id) -> enum ErrorCode {
   (void)power_state;
   (void)entry_point_address;
   (void)context_id;
@@ -226,13 +241,11 @@ static __always_inline auto CpuSuspend(PowerState power_state,
 
 /**
  * Power down the calling core
- * @return ReturnValue 返回值
+ * @return ErrorCode 错误码
  * @see DEN0022F.b_Power_State_Coordination_Interface.pdf#5.1.3
  */
-static __always_inline auto CpuOff() -> enum ReturnValue {
-  Arm_smccc_res res;
-  SecureMonitorCall(kCPU_OFF, 0, 0, 0, 0, 0, 0, 0, &res);
-  return (ReturnValue)res.a0;
+static __always_inline auto CpuOff() -> enum ErrorCode {
+  return (ErrorCode)SecureMonitorCall(kCPU_OFF, 0, 0, 0, 0, 0, 0, 0).a0;
 }
 
 /**
@@ -246,16 +259,16 @@ static __always_inline auto CpuOff() -> enum ReturnValue {
  * Bits[0:7] Aff0: Match Aff0 of target core MPIDR
  * @param entry_point_address 启动地址
  * @param context_id 上下文标识
- * @return ReturnValue 返回值
+ * @return ErrorCode 错误码
  * @see DEN0022F.b_Power_State_Coordination_Interface.pdf#5.1.4
  */
 static __always_inline auto CpuOn(uint64_t target_cpu,
                                   uint64_t entry_point_address,
-                                  uint64_t context_id) -> enum ReturnValue {
-  Arm_smccc_res res;
-  SecureMonitorCall(kCPU_ON_64, target_cpu, entry_point_address, context_id, 0,
-                    0, 0, 0, &res);
-  return (ReturnValue)res.a0;
+                                  uint64_t context_id) -> enum ErrorCode {
+  return (ErrorCode)SecureMonitorCall(kCPU_ON_64, target_cpu,
+                                      entry_point_address, context_id, 0, 0, 0,
+                                      0)
+      .a0;
 }
 
 }  // namespace psci
