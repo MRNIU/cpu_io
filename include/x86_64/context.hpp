c24a38290a78a28ee91cfe75a1b77405ab5929b4
Niu Zhihong <zhihong@nzhnb.com>
2026-01-19 21:06:20 +0800

    feat: CPU_IO_INCLUDE_X86_64_CONTEXT_HPP_
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

---
+++ include/x86_64/context.hpp
@@ -0,0 +1,22 @@
+/**
+ * @copyright Copyright The cpu_io Contributors
+ */
+
+#ifndef CPU_IO_INCLUDE_X86_64_CONTEXT_HPP_
+#define CPU_IO_INCLUDE_X86_64_CONTEXT_HPP_
+
+#include <cstdint>
+
+namespace cpu_io {
+
+struct TrapContext {
+  /// @todo
+};
+
+struct CalleeSavedContext {
+  /// @todo
+};
+
+}  // namespace cpu_io
+
+#endif  // CPU_IO_INCLUDE_X86_64_CONTEXT_HPP_
--- include/x86_64/cpu.hpp
+++ include/x86_64/cpu.hpp
@@ -11,6 +11,7 @@
 #include <type_traits>
 
 #include "apic.hpp"
+#include "context.hpp"
 #include "cpuid.hpp"
 #include "io.hpp"
 #include "msr.h"
